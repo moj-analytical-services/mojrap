e226513041de16c2308ce922195948356dd6e4ee
ms130 <marco.surace@justice.gov.uk>
2020-12-17 11:26:40 +0000

    Updated description file with openxlsx and fixed test_write_formatted_table.R

--- tests/testthat/test_write_formatted_table.R
+++ tests/testthat/test_write_formatted_table.R
@@ -2,7 +2,7 @@
 wb <- openxlsx::createWorkbook()
 openxlsx::addWorksheet(wb, sheetName = "sheet1")
 
-save_formatted_table(workbook = wb,
+write_formatted_table(workbook = wb,
                      sheet_name = "sheet1",
                      tables = list(mtcars[1:3, 1:3], "Cars", mtcars[4:6, 1:5]),
                      notes = c("a", "b"),
@@ -11,15 +11,15 @@ save_formatted_table(workbook = wb,
 
 #Save to temporary file
 tmp_file <- tempfile(fileext = ".xlsx")
-saveWorkbook(wb = wb, file = tmp_file, overwrite = TRUE)
+openxlsx::saveWorkbook(wb = wb, file = tmp_file, overwrite = TRUE)
 
 
 test_that("Tables write in the expected position on a sheet", {
 
   #Check values expected at various points in the sheet
-  expect_equal(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[1,1], "21")
-  expect_equal(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[7,4], 110)
-  expect_equal(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[12,1], "b")
+  expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[1,1], "21")
+  expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[7,4], 110)
+  expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[12,1], "b")
 
 })
 
@@ -27,18 +27,18 @@ test_that("Tables write in the expected position on a sheet", {
 test_that("Header rows write in the correct way on a sheet", {
 
   #Check header is in the right position, and there is a blank column above and below
-  expect_equal(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[5,1], "Cars")
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[4,1]))
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[6,1]))
+  expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[5,1], "Cars")
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[4,1]))
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[6,1]))
 
 })
 
 
 test_that("Notes write in the correct place", {
   #Check notes are in the right position, and there is a blank column above
-  expect_equal(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[12,1], "b")
-  expect_equal(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[11,1], "a")
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[10,1]))
+  expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[12,1], "b")
+  expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[11,1], "a")
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE, skipEmptyRows = FALSE)[10,1]))
 
 })
 
@@ -51,9 +51,9 @@ test_that("Row merge on notes works correctly", {
                                    skipEmptyRows = FALSE, fillMergedCells = TRUE)[11,2], "a")
   expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
                                    skipEmptyRows = FALSE, fillMergedCells = TRUE)[11,3], "a")
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE,
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
                               skipEmptyRows = FALSE, fillMergedCells = TRUE)[11,4]))
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE,
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
                               skipEmptyRows = FALSE, fillMergedCells = TRUE)[11,5]))
 
   expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
@@ -62,9 +62,9 @@ test_that("Row merge on notes works correctly", {
                                    skipEmptyRows = FALSE, fillMergedCells = TRUE)[12,2], "b")
   expect_equal(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
                                    skipEmptyRows = FALSE, fillMergedCells = TRUE)[12,3], "b")
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE,
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
                               skipEmptyRows = FALSE, fillMergedCells = TRUE)[12,4]))
-  expect_true(is.na(read.xlsx(tmp_file, colNames = FALSE,
+  expect_true(is.na(openxlsx::read.xlsx(tmp_file, colNames = FALSE,
                               skipEmptyRows = FALSE, fillMergedCells = TRUE)[12,5]))
 
 })
@@ -76,13 +76,13 @@ test_that("Not giving a list of tables throws an error", {
   openxlsx::addWorksheet(a, sheetName = "sheet1")
 
   #Check that it returns an error, and that it returns a specific message
-  expect_error(save_formatted_table(workbook = a,
+  expect_error(write_formatted_table(workbook = a,
                                     sheet_name = "sheet1",
                                     tables = mtcars[1:3, 1:3],
                                     notes = c("a", "b"),
                                     starting_row = 3,
                                     quarterly_format = NULL))
-  expect_error(save_formatted_table(workbook = a,
+  expect_error(write_formatted_table(workbook = a,
                                     sheet_name = "sheet1",
                                     tables = mtcars[1:3, 1:3],
                                     notes = c("a", "b"),
@@ -96,14 +96,14 @@ test_that("Not giving a vector of notes throws an error", {
   openxlsx::addWorksheet(a, sheetName = "sheet1")
 
   #Check that it returns an error, and that it returns a specific message
-  expect_error(save_formatted_table(workbook = a,
+  expect_error(write_formatted_table(workbook = a,
                                     sheet_name = "sheet1",
                                     tables = list(mtcars[1:3, 1:3]),
                                     notes = list("a", "b"),
                                     starting_row = 3,
                                     quarterly_format = NULL))
 
-  expect_error(save_formatted_table(workbook = a,
+  expect_error(write_formatted_table(workbook = a,
                                     sheet_name = "sheet1",
                                     tables = list(mtcars[1:3, 1:3]),
                                     notes = list("a", "b"),
@@ -112,13 +112,13 @@ test_that("Not giving a vector of notes throws an error", {
 })
 
 test_that("Not giving a wb or sheet throws an error", {
-  expect_error(save_formatted_table(sheet_name = "sheet1",
+  expect_error(write_formatted_table(sheet_name = "sheet1",
                        tables = list(mtcars[1:3, 1:3]),
                        notes = c("a", "b"),
                        starting_row = 3,
                        quarterly_format = NULL))
 
-  expect_error(save_formatted_table(workbook = wb,
+  expect_error(write_formatted_table(workbook = wb,
                                     tables = list(mtcars[1:3, 1:3]),
                                     notes = c("a", "b"),
                                     starting_row = 3,
